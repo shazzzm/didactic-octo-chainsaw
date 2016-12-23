c92b5b42fdbf6be78a5e836fb914585961d5ad75
Tristan Millington <tristan.millington@gmail.com>
2016-12-23 22:40:51 +0000

    Added method to move x around screen

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import './App.css';
 class Square extends React.Component {
   render() {
     return (
-      <button className="square" onClick={() => this.props.onClick()}>
+      <button className="square">
         {this.props.value}
       </button>
     );
@@ -16,19 +16,46 @@ class Board extends React.Component {
   constructor() {
     super();
     this.state = {
-      board_width:  3,
-      board_length: 3,
+      location_x: 0,
+      location_y: 0,
+      board_width:  4,
+      board_length: 4,
       squares: Array(9).fill(null)
     };
-  }
-  renderSquare(i) {
-    return <Square value={this.state.squares[i]} onClick={() => this.handleClick(i)} />;
+    this.handleKeyPress = this.handleKeyPress.bind(this)
+
   }
 
-  renderRow(start, row_size) {
+  renderSquare(contains_x) {
+    return <Square value={contains_x} />;
+  }
+
+  move(x, y) {
+    console.log("Move involked!")
+    var new_x = this.state.location_x + x;
+    var new_y = this.state.location_y + y;
+    // Perform some bounds checking
+    if (new_x >= 0 && new_y >= 0) {
+      console.log("Moving x")
+      console.log(new_x)
+      console.log(new_y)
+      this.setState({"location_x" : new_x, "location_y" : new_y})
+    }
+  }
+
+  renderRow(row_y, row_size) {
     var rows = [];
+    console.log("Rendering row")
+    console.log(this.state.location_x)
+    console.log(this.state.location_y)
     for (var i=0; i < row_size; i++) {
-        rows.push(this.renderSquare(start+i));
+        var mark = null;
+        if (row_y == this.state.location_y && i == this.state.location_x) {
+          mark = 'X';
+
+          console.log("X should be here")
+        }
+        rows.push(this.renderSquare(mark));
     }
     return (
       <div className="board-row">
@@ -41,24 +68,26 @@ class Board extends React.Component {
     var rows = [];
     var count = 0;
     for (var i=0; i < this.state.board_length; i++) {
-      rows.push(this.renderRow(i*this.state.board_length, this.state.board_width));
+      rows.push(this.renderRow(i, this.state.board_width));
     }
 
     return (
-      <div>
+     <div id="blah" onKeyUp={this.handleKeyPress}>
         {rows}
       </div>
     );
   }
-  
-  handleClick(i) {
-    const squares = this.state.squares.slice();
-    if (squares[i] === 'X') {
-      squares[i] = ''
-    } else {
-      squares[i] = 'X';
+
+  handleKeyPress(event) {
+    if (event.key == 'w') {
+      this.move(0, -1);
+    } else if (event.key == 's') {
+      this.move(0, 1);
+    } else if (event.key == 'd') {
+      this.move(1, 0);
+    } else if (event.key == 'a') {
+      this.move(-1, 0);
     }
-    this.setState({squares: squares});
   }
 }
 
